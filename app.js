944d7fbc54bcdf17481f457f09f231301dbae3f7
remig <urbaniec.remi@gmail.com>
2019-09-17 17:11:42 +0200

    testpushTrannois

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const webpush = require('web-push');
-const vapidKeys = require('./NOTversioned/vapidKeys.js');
+const vapidKeys = require('./NOTversioned/vapid.js');
 
 // initialise le module webpush avec les clefs VAPId et les informations de contact
 webpush.setVapidDetails(

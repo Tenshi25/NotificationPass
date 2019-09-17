2e2a88688c47d6385f990b5fe35afce3bf9ec43b
remig <urbaniec.remi@gmail.com>
2019-09-17 17:21:21 +0200

    testpushTrannois2

---
+++ NOTversioned/vapid.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "subject": "mailto:<someone@example.com>",
+  "publicKey": "BPjT3wBtqa_4hUx0ODMmGsmA2v_uMVcH6IRLjaW3HlTZNacrKoXLWjyF1XA6fHj_gbc6N7gd4NM1x1UxCwsMfMk",
+  "privateKey": "kX56xE56wcEL8o-PL6EGaPLLsmb1OgrYcBeSPMwM__I"
+}
